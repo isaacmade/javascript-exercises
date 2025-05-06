b539cb69be173091649852fbb712492271bf8495
Isaac Key <175264936+isaacmade@users.noreply.github.com>
2025-05-07 09:04:02 +1000

    finished exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return ("ERROR");
+    } else {
+        let finalString = "";
+        for (let i = 0; i < num; i++) {
+            // finalString += string;
+            finalString = finalString + string;
+        }
+        return finalString;
+    }
+}
 // Do not edit below this line
 module.exports = repeatString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+    let finalSum = 0;
+    if (start < 0 || finish < 0 || !Number.isInteger(start) || !Number.isInteger(finish)) {
+        return 'ERROR'
+    } else if (start < finish) {
+        for (let i = start; i <= finish; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    } else {
+        for (let i = finish; i <= start; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    }
 };
 
 // Do not edit below this line
